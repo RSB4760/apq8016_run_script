3901afa876047ba2c0f2f888bdfa40590bcdfc9e
Alleen Wang <alleen.wang@advantech.com.tw>
2017-08-02 16:42:34 +0800

    Modify for advan_20170801_002.

--- apq8016_download.sh
+++ apq8016_download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-repo init -u https://github.com/RSB4760/apq8016_manifest_android -b LA.BR.1.2.7-01010-8x16_advan -m advan/LA.BR.1.2.7-01010-8x16.0_advan.xml
+repo init -u https://github.com/RSB4760/apq8016_manifest_android -b advan_20170801_002 -m advan/LA.BR.1.2.7-01010-8x16.0_advan.xml
 repo sync -j20
-repo forall -c git checkout -b LA.BR.1.2.7-01010-8x16_advan advantech-github/LA.BR.1.2.7-01010-8x16_advan
+repo forall -c git checkout -b advan_20170801_002 tags/advan_20170801_002
 
